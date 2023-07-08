30bcad64c09313bc4c510596c28f65cf39c8199c
alexismcode <alexismcode@gmail.com>
2023-07-08 17:53:00 -0500

    Agregar archivos de proyecto.

---
+++ BaseProduct.Logica/Producto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseProduct.Logica
+{
+    public class Producto
+    {
+        public string? Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int CantidadDisponible { get; set; }
+
+        public decimal CalcularCostoTotal()
+        {
+            return Precio * CantidadDisponible;
+        }
+
+        public void ActualizarCantidad(int cantidadComprada)
+        {
+            CantidadDisponible -= cantidadComprada;
+        }
+    }
+}
+
---
+++ BaseProduct/Program.cs
@@ -0,0 +1,34 @@
+﻿using BaseProduct.Logica;
+
+try
+{
+    Producto producto = CrearProducto();
+
+    Console.WriteLine("Nombre: " + producto.Nombre);
+    Console.WriteLine("Precio: " + producto.Precio);
+    Console.WriteLine("Cantidad disponible: " + producto.CantidadDisponible);
+
+    Console.WriteLine("Realizar una compra:");
+    Console.Write("Ingrese la cantidad deseada: ");
+    int cantidadComprada = Convert.ToInt32(Console.ReadLine());
+
+    producto.ActualizarCantidad(cantidadComprada);
+    Console.WriteLine("Cantidad disponible después de la compra: " + producto.CantidadDisponible);
+    Console.WriteLine("Costo total de los productos disponibles: " + producto.CalcularCostoTotal());
+
+    Console.ReadLine();
+
+    Producto CrearProducto()
+    {
+        Producto producto = new Producto();
+        producto.Nombre = "Camiseta";
+        producto.Precio = 20.00m;
+        producto.CantidadDisponible = 10;
+
+        return producto;
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR:....... [{ex.Message}]");
+}
